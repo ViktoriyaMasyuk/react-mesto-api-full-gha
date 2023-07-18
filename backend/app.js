8834539fd49e56b10f1688fb59075d321530425d
Viktoriya Masyuk <viktoriyamasyuk@yandex.ru>
2023-07-19 00:34:49 +0500

    fff

--- backend/app.js
+++ backend/app.js
@@ -28,7 +28,7 @@ app.use(errorLogger);
 app.use(errors());
 app.use(serverError);
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
