30296d9a7bfc8f49e3ac32415a0d57257406579d
Viktoriya Masyuk <viktoriyamasyuk@yandex.ru>
2023-07-21 00:24:42 +0500

    fff

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -15,7 +15,7 @@ module.exports = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'super-strong-secret');
     req.user = payload;
   } catch (err) {
     throw new UnauthorizedError('Необходима авторизация');
