1dde94795c340921365ce7ff51185d72601a1b6b
Viktoriya Masyuk <viktoriyamasyuk@yandex.ru>
2023-07-21 19:04:10 +0500

    fff

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -12,15 +12,14 @@ const cors = (req, res, next) => {
   const { method } = req;
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   const requestHeaders = req.headers['access-control-request-headers'];
-
+  res.header('Access-Control-Allow-Credentials', true);
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', true);
   }
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
-    return res.end();
+    res.end();
   }
   return next();
 };
